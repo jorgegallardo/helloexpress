c6f7e7d0db9ba0b45fa5efb06f29f8510b1af436
Jorge Gallardo <jorge8486@gmail.com>
2014-02-18 03:03:03 -0500

    routes

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
-var path = require('path');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function() {
+	app.set('port', process.env.PORT || 3000);
+	app.use(express.json());
+	app.use(express.urlencoded());
+});
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+//default route
+app.get("/", function(req, res) {
+	res.send("Hello, Express!");
+});
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get("/hi", function(req, res) {
+	var message = [
+		"<h1>Hello, Express</h1>",
+		"<p>Welcome to the Golden Nugget!</p>"].join("\n");
+	res.send(message);
+});
 
-http.createServer(app).listen(app.get('port'), function(){
+// app.get("/users/:userId", function(req, res) {
+// 	res.send("<h1>Hello, User #" + req.params.userId + "!");
+// });
+
+app.post("/users", function(req, res) {
+	res.send("Creating a new user with the name " + req.body.username + ".");
+});
+
+app.get(/\/users\/(\d*)\/?(edit)?/, function(req, res) {
+	// /users/10
+	// /users/10/
+	// /users/10/edit
+	var message = "user #" + req.params[0] + "'s profile";
+	if(req.params[1] === 'edit') {
+		message = "Editing " + message;
+	} else {
+		message = "Viewing " + message;
+	}
+	res.send(message);
+});
+
+http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
